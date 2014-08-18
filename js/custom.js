4623adf32d4acbc7034051dfbae36537485a6121
Eduardo Blancas Reyes <edu@cacaolabs.co>
2014-08-18 12:30:58 -0500

    fixes form

--- js/custom.js
+++ js/custom.js
@@ -225,24 +225,47 @@ $("#contact").submit(function (e) {
     var message = $("#message").val();
     var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
 
-    function isValidEmail(emailAddress) {
+    function validEmail(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
     };
 
-    if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-        $.ajax({
+    var validMessage = (message.length > 1);
+    var validName = (name.length > 1);
+
+    if(!validEmail(email)){
+        $('.error_email').fadeIn(1000);
+    }else{
+        $('.error_email').fadeOut(1000);
+    }
+
+    if(!validMessage){
+        $('.error_message').fadeIn(1000);
+    }else{
+        $('.error_message').fadeOut(1000);
+    }
+
+    if(!validName){
+        $('.error_name').fadeIn(1000);
+    }else{
+        $('.error_name').fadeOut(1000);
+    }
+
+    if (validEmail(email) && validMessage && validName) {
+        var request = $.ajax({
             type: "POST",
-            url: "sendmail.php",
+            url: "http://getsimpleform.com/messages?form_api_token=0a6a2644f82ae197fe8b48678532cca0",
             data: dataString,
-            success: function () {
-                $('.success').fadeIn(1000);
-                $('.error').fadeOut(500);
-            }
         });
-    } else {
-        $('.error').fadeIn(1000);
-        $('.success').fadeOut(500);
+
+        request.always(function() {
+            $('.success').fadeIn(1000);
+
+            $("#name").val('')
+            $("#email").val('')
+            $("#message").val('')
+        });
+
     }
 
     return false;
